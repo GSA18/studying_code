5f8b8d01eb1c08ee5ba3037f8bd23035f9e2074e
Gustavo Sobreiro de Assis <gustavo.sobreiro@olist.com>
2021-02-17 18:59:55 -0300

    Fix APi

--- view_api/api_flask.py
+++ view_api/api_flask.py
@@ -1,17 +1,21 @@
 import sys
 sys.path.append('.')
-from flask import Flask
-from flask_restful import Api
 from resources.product_pet_resource import Product_petResource
+from flask_restful import Api
+from flask import Flask
+
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Product_petResource, '/product_pet', endpoint='product_pets')
-api.add_resource(Product_petResource, '/product_pet/<int:id>', endpoint='product_pet')
+api.add_resource(Product_petResource, '/product_pet/<int:id>',
+                 endpoint='product_pet')
+
 
 @app.route('/')
 def main():
+
     return 'Ta funcionando corretamente'
 
 
